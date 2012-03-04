f557c916c49ebfe912ee242c6415594b7bc4d712
Hugo Duncan <hugo@hugoduncan.org>
2012-03-04 08:16:57 -0500

    Update project.clj to version 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject swell "0.1.0"
+(defproject swell "0.2.0-SNAPSHOT"
   :description "A CL style restart library for clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [slingshot "0.10.2"]]
